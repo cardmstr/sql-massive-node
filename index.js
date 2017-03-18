da19fb30c160637cf464c25029ff7297a8fb45b5
Spencer Simons <spencersimons@Spencers-MacBook-Pro.local>
2017-03-18 13:09:19 -0600

    finished scaffolding of app, running

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var bodyparser = require('body-parser');
+var massive = require('massive');
+
+var port = process.env.PORT || 3000;
+var connString = "postgres://spencersimons@localhost/testProducts";
+var massInst = massive.connectSync({connectionString: connString});
+
+var app = module.exports = express();
+app.use(bodyparser.json());
+app.set('db', massInst);
+
+var db = app.get('db');
+
+
+
+
+app.listen(port ,function(){
+  console.log('sql-massive-node running on port ' + port);
+})
